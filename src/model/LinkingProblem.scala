9ac84b2e70980c00da5acda0cb386687d8f9663b
yantangucsb <tangyan0627@gmail.com>
2015-05-07 18:10:12 -0700

    more more

--- src/model/LinkingProblem.scala
+++ src/model/LinkingProblem.scala
@@ -15,55 +15,5 @@ class LinkingProblem(input: String, filename: String) {
   var sourceFilename: String = filename
   var text: String = input
   //TF_IDF?
-  var compponents: List[Mention] = List()
-  var entityMap: Map[String, Mention] = MentionExtract.generateMention(types)(text, generateConstituents())
-
-  def types(c: Constituent): List[SurfaceType.types] = {
-    List(SurfaceType.NPSubchunk);
-  }
-  
-  def generateConstituents(): List[Constituent] = {
-    var nerTagger: NERTagger = new NERTagger()
-    nerTagger.setUp()
-    nerTagger.tagData(text)
-    var shallowparser = new ShallowParser("configs/NER.config")
-    shallowparser.performChunkerAndPos(text)
-
-    var constituents: List[Constituent] = List()
-    //get label of ner tags
-    var chunkerLabels: Labeling = TextLabeler.getLabeling(shallowparser.chunkerWords, shallowparser.chunkerTags, text, "IllinoisChunker")
-    var label: Iterator[Span] = chunkerLabels.getLabelsIterator().asScala
-    var ta: TextAnnotation = createAnnotation()
-    while(label.hasNext){
-      var span: Span = label.next();
-      var c: Constituent = makeConstituentFixTokenBoundaries(span.label, ViewNames.NER, ta, span.start, span.ending);
-      if(c!=null) constituents = constituents ++ List(c);
-    }
-    constituents
-  }
-
-  def makeConstituentFixTokenBoundaries(label: String, viewName: String, ta: TextAnnotation, start: Int, end: Int): Constituent = {
-    var e: Int = end - 1;
-    if (e < start)
-      e = start;
-    var c: Constituent = null;
-    try {
-      c = new Constituent(label, viewName, ta, ta.getTokenIdFromCharacterOffset(start), ta.getTokenIdFromCharacterOffset(e) + 1);
-      if (ta.getText().substring(start, end).equals(c.getSurfaceString()))
-        return c;
-
-      c = new Constituent(label, viewName, ta, ta.getTokenIdFromCharacterOffset(start), ta.getTokenIdFromCharacterOffset(end));
-      if (ta.getText().substring(start, end).equals(c.getSurfaceString()))
-        return c;
-      c = new Constituent(label, viewName, ta, ta.getTokenIdFromCharacterOffset(start), ta.getTokenIdFromCharacterOffset(end) + 1);
-      if (ta.getText().substring(start, end).equals(c.getSurfaceString()))
-        c
-    } catch {
-      case ex: Exception => throw new Exception
-    }
-    null
-  }
-  
-  def createAnnotation(): TextAnnotation = 
-    new TextAnnotation("fakeCorpus","fakeId", text,SentenceViewGenerators.LBJSentenceViewGenerator)
+  var compponents: List[Mention] = MentionExtract.extract(this)
 }
--- src/preprocessing/MentionExtract.scala
+++ src/preprocessing/MentionExtract.scala
@@ -1,12 +1,135 @@
 package preprocessing
 
-import model.Mention
-import model.SurfaceType
+import model._
+
+import edu.illinois.cs.cogcomp.edison.sentences.TokenizerUtilities.SentenceViewGenerators;
+import edu.illinois.cs.cogcomp.edison.sentences.ViewNames;
+import edu.illinois.cs.cogcomp.edison.sentences.TextAnnotation;
 import edu.illinois.cs.cogcomp.edison.sentences.Constituent;
+import edu.illinois.cs.cogcomp.thrift.base.Labeling;
+import edu.illinois.cs.cogcomp.thrift.base.Span;
+import scala.collection.JavaConverters._
 
 object MentionExtract {
+  def extract(problem: LinkingProblem): List[Mention] = {
+    var NerConstituents: List[Constituent] = generateNerConstituents(problem.text)
+    var NerCandidateEntities: Map[String, Mention] = 
+      generateMention(NerTypes)(problem.text, NerConstituents)
+    var ChunkConstituents: List[Constituent] = generateChunkConstituents(problem.text)
+    var ChunkCandidateEntities: Map[String, Mention] =
+      generateMention(NerTypes)(problem.text, ChunkConstituents)
+    //Merger maps
+    for (e <- NerCandidateEntities.values) {
 
-  //get into the linkingproblem class
+            if ((!e.isTopLevelMention() && GlobalParameters.stops.isStopword(e.surfaceForm.toLowerCase())) 
+                    // Fast unlink
+                    || GlobalParameters.wikiAccess.isKnownUnlinkable(e.surfaceForm)) {
+                continue;
+            }
+
+            // problem is slow
+            List<WikiMatchData> matchData = CandidateGenerator.getGlobalCandidates(ta, e);
+
+            // Here we allow zero candidate entities
+            if (matchData.size() > 0 
+                    || e.isTopLevelMention()
+                    || TitleNameIndexer.normalize(e.surfaceForm) != null) {
+
+                e.generateLocalContext(text, wikipedia);
+
+                e.candidates = Lists.newArrayList();
+                List<WikiCandidate> firstLayer = Lists.newArrayList();
+                for (WikiMatchData wikiMatchData : matchData) {
+                    WikiCandidate candidate = new WikiCandidate(e, wikiMatchData);
+                    firstLayer.add(candidate);
+                }
+                e.candidates.add(firstLayer);
+
+                // now mark the corresponding reference instance (if exists) that problem wikifiable
+                // entity tries to disambiguate it
+                if (refInstanceMap.containsKey(e.getPositionHashKey()))
+                    refInstanceMap.get(e.getPositionHashKey()).setAssignedEntity(e);
+                res.add(e);
+            } else {
+                // there was no wiki match data for the entity
+                if (manualEntities.contains(e)) {
+                    System.out.println("Important warning: the manually defined entity " + e.surfaceForm
+                            + " had 0 disambiguation candidates");
+                }
+
+            }
+        } // running on all potentially the wikifiable entitites
+
+        if (displayProgress) {
+            printProgress(res);
+        }
+
+        System.out.println("Done constructing the Wikifiable entities");
+        expandNER(res);
+        return res;
+  }
+  
+  def generateNerConstituents(text: String): List[Constituent] = {
+    var nerTagger: NERTagger = new NERTagger()
+    nerTagger.setUp()
+    nerTagger.tagData(text)
+
+    var constituents: List[Constituent] = List()
+    //get label of ner tags
+    
+    constituents
+  }
+  
+  def generateChunkConstituents(text: String): List[Constituent] = {
+    var shallowparser = new ShallowParser("configs/NER.config")
+    shallowparser.performChunkerAndPos(text)
+
+    var constituents: List[Constituent] = List()
+    //get label of ner tags
+    var chunkerLabels: Labeling = TextLabeler.getLabeling(shallowparser.chunkerWords, shallowparser.chunkerTags, text, "IllinoisChunker")
+    var label: Iterator[Span] = chunkerLabels.getLabelsIterator().asScala
+    var ta: TextAnnotation = createAnnotation(text)
+    while(label.hasNext){
+      var span: Span = label.next();
+      var c: Constituent = makeConstituentFixTokenBoundaries(span.label, ViewNames.NER, ta, span.start, span.ending);
+      if(c!=null) constituents = constituents ++ List(c);
+    }
+    constituents
+  }
+  
+  def createAnnotation(text: String): TextAnnotation = 
+    new TextAnnotation("fakeCorpus","fakeId", text,SentenceViewGenerators.LBJSentenceViewGenerator)
+
+  def makeConstituentFixTokenBoundaries(label: String, viewName: String, ta: TextAnnotation, start: Int, end: Int): Constituent = {
+    var e: Int = end - 1;
+    if (e < start)
+      e = start;
+    var c: Constituent = null;
+    try {
+      c = new Constituent(label, viewName, ta, ta.getTokenIdFromCharacterOffset(start), ta.getTokenIdFromCharacterOffset(e) + 1);
+      if (ta.getText().substring(start, end).equals(c.getSurfaceString()))
+        return c;
+
+      c = new Constituent(label, viewName, ta, ta.getTokenIdFromCharacterOffset(start), ta.getTokenIdFromCharacterOffset(end));
+      if (ta.getText().substring(start, end).equals(c.getSurfaceString()))
+        return c;
+      c = new Constituent(label, viewName, ta, ta.getTokenIdFromCharacterOffset(start), ta.getTokenIdFromCharacterOffset(end) + 1);
+      if (ta.getText().substring(start, end).equals(c.getSurfaceString()))
+        c
+    } catch {
+      case ex: Exception => throw new Exception
+    }
+    null
+  }
+  
+  def NerTypes(c: Constituent): List[SurfaceType.types] = {
+    List(SurfaceType.NER, SurfaceType.withName(c.getLabel()))
+  }
+  
+  def ChunkTypes(c: Constituent): List[SurfaceType.types] = {
+    List(SurfaceType.NPSubchunk)
+  }
+  
   def generateMention(types: Constituent => List[SurfaceType.types])(input: String, candidates: List[Constituent]): Map[String, Mention] = {
     var entityMap: Map[String, Mention] = Map()
     for (c <- candidates) {
